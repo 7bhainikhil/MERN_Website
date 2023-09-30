9b6cfac0bc9f57b2568dde8e93f2d1363b588242
nikhilsatbhai7 <nikhilsatbhai7@gmail.com>
2023-10-01 00:16:22 +0530

    App.js and App.css

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Profile from './components/Event';
+import Login from './components/Login';
+import Service from './components/Service'
+import Contact from './components/Contact';
+import About from './components/About'
+import Reviews from './components/Reviews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/reviews" element={<Reviews/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
